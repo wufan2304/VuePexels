3fc2ec207270ebc88560e72c2d0e091a71eb6c1c
wufan2304 <wufan-2304@foxmail.com>
2017-08-18 23:30:14 +0800

    vue pexels init the project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <waterfall :line-gap="200" :watch="items">
+      <!-- each component is wrapped by a waterfall slot -->
+      <waterfall-slot v-for="(item, index) in items" :width="item.width" :height="item.height" :order="index" :key="item.id">
+        <!--
+        your component
+      -->
+      </waterfall-slot>
+    </waterfall>
+  </div>
+</template>
+
+<script>
+import Waterfall from 'vue-waterfall/lib/waterfall'
+import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      pictures: []
+    }
+  },
+  components: {
+    Waterfall,
+    WaterfallSlot
+  },
+  mounted: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
